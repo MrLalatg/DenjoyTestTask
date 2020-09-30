318443aa4359c9285237c3456970a906a5710db2
Ilya <MrLalatg@gmail.com>
2020-09-30 15:51:01 +0300

    Implemented simple authentification

--- server.js
+++ server.js
@@ -1,14 +1,43 @@
 const express = require('express');
 const sha256 = require('sha256');
 const bodyParser = require('body-parser');
-const exphbs = require('express-handlebars');
 const path = require('path');
-const methodOverride = require('method-override');
-const redis = require('redis');
+const fs = require('fs');
+const crypto = require('crypto');
 
 const app = express();
 const port = process.env.PORT || "80";
 
-app.get('/authorize', (req, res) => {
+const config_path = path.join(__dirname + '/config.json');
+const config = JSON.parse(fs.readFileSync(config_path));
+let logged_in = {};
 
+app.use(bodyParser.json());
+
+app.post('/auth', (req, res) => {
+  if (req.body.login in config.users){
+    if (req.body.login in logged_in){
+      if (logged_in[req.body.login] === req.connection.remoteAddress){
+        res.send("Already logged in!");
+      } else {
+        res.send("User connected from another ip!")
+      }
+    } else{
+      if (req.body.password === config.users[req.body.login]){
+        // let current_date = (new Date()).valueOf().toString();
+        // let random = Math.random().toString();
+        // let cookie = crypto.createHash('sha1').update(current_date + random).digest('hex');
+        logged_in[req.body.login] = req.connection.remoteAddress;
+        res.send("Logged in!");
+      } else {
+        res.sendStatus(403);
+      }
+    }
+  } else {
+    res.sendStatus(403);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Listening to requests on http://localhost:${port}`);
 });
