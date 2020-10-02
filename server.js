77aee156ee73561d076439473d75469ba8897404
Ilya <MrLalatg@gmail.com>
2020-10-02 22:42:47 +0300

    Implemented cookies

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const fs = require('fs');
 const crypto = require('crypto');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 const port = process.env.PORT || "80";
@@ -13,22 +14,27 @@ const config = JSON.parse(fs.readFileSync(config_path));
 let logged_in = {};
 
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 app.post('/auth', (req, res) => {
+  let login_successful = false;
   if (req.body.login in config.users){
-    if (req.body.login in logged_in){
-      if (logged_in[req.body.login] === req.connection.remoteAddress){
+    if (req.headers.cookie in logged_in){
+      if (logged_in[req.headers.cookie] === req.body.login){
+        login_successful = true;
         res.send("Already logged in!");
       } else {
-        res.send("User connected from another ip!")
+        delete logged_in[req.headers.cookie];
       }
-    } else{
+    }
+    if (!login_successful){
       if (req.body.password === config.users[req.body.login]){
-        // let current_date = (new Date()).valueOf().toString();
-        // let random = Math.random().toString();
-        // let cookie = crypto.createHash('sha1').update(current_date + random).digest('hex');
-        logged_in[req.body.login] = req.connection.remoteAddress;
-        res.send("Logged in!");
+        let current_date = (new Date()).valueOf().toString();
+        let random = Math.random().toString();
+        let cookie = crypto.createHash('sha1').update(current_date + random).digest('hex');
+        logged_in[cookie] = req.body.login;
+        res.cookie('token', cookie);
+        res.sendStatus(200);
       } else {
         res.sendStatus(403);
       }
@@ -39,14 +45,10 @@ app.post('/auth', (req, res) => {
 });
 
 app.get('/userstatus', (req, res) => {
-  if(req.query.login in config.users){
-    if (req.query.login in logged_in){
-      res.send(`User ${req.query.login} is logged in from ${logged_in[req.query.login]}`);
-    } else {
-      res.send(`User ${req.query.login} is not logged in`);
-    }
+  if (req.cookies.token in logged_in){
+    res.send(`Logged in as ${logged_in[req.cookies.token]}`);
   } else {
-    res.send(`User ${req.query.login} doesn't exitst!`);
+    res.send("You're not logged in!");
   }
 });
 
